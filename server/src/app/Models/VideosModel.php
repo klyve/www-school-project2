effb9a4e9a0ef642d98e32a65fc277afde4015d6
Jonas Johan solsvik <jonasjso@stud.ntnu.no>
2018-04-11 10:20:59 +0200

    VideosModel.php * Added comments and concerns. * New field * New field

--- server/src/app/Models/VideosModel.php
+++ server/src/app/Models/VideosModel.php
@@ -1,20 +1,43 @@
 <?php
 
+
 class VideosModel extends \MVC\Core\Model {
 
   public $table = 'videos';
-  public $id;  
-  public $name;        // ex: '10 biggest rollercoasters in the world'
-  public $description; // ex: 'My trip around the world took me....'
-  public $mimetype;    // ex: 'video/mp4, video/ogg'
-  public $viewCount;   // ex: '12000000'
+
+  // Primary key
+  public $id;          
+
+  // Foreign key
+  public $userid;      
+
+  // text, max 128 chars, '10 biggest rollercoasters in the world'
+  public $name;        
+
+  // text, max 2048 chars, 'My trip around the world took me....'
+  public $description; 
+
+  // integer: 0 - MAXINT
+  public $viewCount;   
 
   // @TODO: Discuss which format the subtitles should be stored as.
   //        Should it be in a separate table to more easily retrieve 
   //        a subtitle without the need for parsing a subtitle format 
   //        during run-time? Does html natively parse .srt so we don't
-  //         have to think about it?   - JSolsvik 11.04.2018
-  public $subtitle;   // ex: .srt formatted subtitle. 
+  //         have to think about it? Should be store the subtitle in a 
+  //        file instead, just like with videos and thumbnails ? 
+  //                                    - JSolsvik 11.04.2018
 
-  public $userid;     // Foreign key to an instance in the UsersModel
+  // Maybe .srt formatted subtitle. 
+  public $subtitle; 
+
+  public $exclude = ['id'];
+
+    // @NOTE: Each entry in this table implicitly points to two files
+    //          (possibly subtitle file also?). 
+    //        One video file and a thumbnail file. The filenames are 
+    //        based on hash of the metadata in an entry.
+    //        Example: videos/<hash($id . $userid . $name)>.mp4
+    //                 thumbnails/<hash($id . $userid . $name)>.png
+    //                 subtitles/<hash($id . $userid . $name)>.srt
 }
