18609dd5c1d7c0ca656f763d88b807a56a7d529b
Eldar Hauge Torkelsen <eldarht@stud.ntnu.no>
2018-04-13 21:19:01 +0200

    Added Model with migration/seeding for Tag table and added signed to allow for character based primarykey

---
+++ server/src/App/Database/Migrations/TagMigrations.php
@@ -0,0 +1,42 @@
+<?php namespace App\Database\Migrations;
+/**
+ * This is a migrator which creates the comments table and sets the fields
+ * name, type, primary key or not and length if applicable.
+ * We use the CLI tool phptoolbox migrate:up to create the table, the phptoolbox migrate:down
+ * to destroy the table and phptoolbox migrate:refresh to run the down function first, then
+ * the up function.
+ *
+ * PHP version 7
+ *
+ *
+ * @category   Migrators
+ * @package    kruskontroll
+ * @version    2.0
+ * @link       https://bitbucket.org/klyve/imt2291-prosjekt2-v2018/
+ * @since      File available since Release 2.0
+ */
+
+use \MVC\Database\Schema;
+use \MVC\Core\Config;
+
+class TagMigrations {
+
+/**
+ * The up function creates a new Tag table with name string as primary key.
+ */
+
+    public function up() {
+        Schema::create('Tag', function($table){
+            $table->primary('name')->signed()->varchar(255);
+        });
+    }
+
+/**
+ * The down function destroys the Tag table.
+ */
+
+    public function down() {
+        Schema::destroy('Tag');
+    }
+
+}
