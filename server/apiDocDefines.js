c452a29e65b484f906212ef8cc6e2f2c51b8f10c
Eldar Hauge Torkelsen <eldarht@stud.ntnu.no>
2018-05-03 19:17:19 +0200

    Added apidoc block to PlaylistTagController

--- server/apiDocDefines.js
+++ server/apiDocDefines.js
@@ -18,3 +18,14 @@
  * Require ownership of resource.
  */
 
+ /**
+ * @apiDefine data
+ * @apiSuccess {String} data Success message.
+ */
+ 
+ /**
+ * @apiDefine errorCode
+ * @apiError {String} code Http status code.
+ * @apiError {String} error Internal error usefull for debug.
+ * @apiError {String} message Error message.
+ */
